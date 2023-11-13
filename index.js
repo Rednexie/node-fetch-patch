b750f58acac1bbbf66e02a920e0504909278c2de
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-11-13 22:35:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const fetch = require("./node-fetch-patch");
+const fetch = require("../node-fetch-patch")
+if(fetch.patchType){
+    if(fetch.patchType === 0) console.log('using nodejs native fetch api')
+    if(fetch.patchType === 2) console.log('using node-fetch with require')
+    if(fetch.patchType === 3) console.log('using node-fetch with import')
+}
+else{
+    console.log('no fetch api found, installing...')
+}
