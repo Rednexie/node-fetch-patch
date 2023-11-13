7699ad0d1ee06d9c412b2ed3b7259c5e09824a71
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-11-13 22:36:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ if(fetch.patchType){
     if(fetch.patchType === 2) console.log('using node-fetch with require')
     if(fetch.patchType === 3) console.log('using node-fetch with import')
 }
+else{
+    console.log('no fetch installation found')
+}
